2fdad0e3ae3559d9daa5b0baf6cad00e796422fa
ners <ners@gmx.ch>
2023-06-23 17:44:37 +0200

    initialise lsp_lines

--- modules/home/neovim/plugins/lsp.nix
+++ modules/home/neovim/plugins/lsp.nix
@@ -4,9 +4,20 @@
   programs.neovim.plugins = with pkgs.vimPlugins; [
     lsp-inlayhints-nvim
     lsp_extensions-nvim
-    lsp_lines-nvim
     lsp_signature-nvim
     lspkind-nvim
     nvim-lspconfig
+    {
+      plugin = lsp_lines-nvim;
+      type = "lua";
+      config = ''
+        -- Disable virtual_text since it's redundant due to lsp_lines.
+        vim.diagnostic.config({
+          virtual_text = false,
+        })
+
+        require("lsp_lines").setup()
+      '';
+    }
   ];
 }
