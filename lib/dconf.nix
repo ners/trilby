ececfa5cf9d086efdfd28c1b316d42ca8e4a9197
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2023-11-26 16:43:20 +0100

    fix integer types in dconf profile; fixes #10 (#12)
    * fix integer types in dconf profile; fixes #10
    * fix a bug in dconfFlatten; make flattenAttrs take a cond by default

--- lib/dconf.nix
+++ lib/dconf.nix
@@ -5,11 +5,12 @@ with lib;
 {
   dconfFlatten =
     let
+      cond = x: not (hasAttr "_type" x);
       merge = { name, value }:
         assert assertMsg
           (length name > 1)
           "dconf configuration requires at least two-level names!";
         nameValuePair (concatStringsSep "/" (init name)) { ${last name} = value; };
     in
-    flattenAttrsWith merge;
+    flattenAttrsWith cond merge;
 }
