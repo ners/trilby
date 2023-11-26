e101a370d27cb60f7428d2392cedea3c79f68c8f
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2023-11-26 15:07:53 +0100

    fix a bug in dconfFlatten; make flattenAttrs take a cond by default

--- lib/attrsets.nix
+++ lib/attrsets.nix
@@ -16,24 +16,25 @@ with lib;
     assert assertMsg (isAttrs attrs) attrs;
     attrs // { name = f attrs.name; };
 
-  # Flatten an attrset by bringing all the leaves to the top level.
-  # As there is no singular name flattening strategy, the result is a list of
+  # Flatten an attrset by bringing all the leaves to the top level. As there is
+  # no singular name flattening strategy, the result is a list of
   # NameValuePairs, where the name is a list of parts.
   #
+  # The 'cond' predicate function specifies whether to recurse. Use
+  # 'const true' to recurse all the way down.
+  #
   # Example:
-  # flattenAttrs { a = { b = 3; c = true; }; d = "paramecium"; }
+  # flattenAttrs (const true) { a = { b = 3; c = true; }; d = "paramecium"; }
   # => [ { name = ["a" "b"]; value = 3; } { name = ["a" "c"]; value = true; } { name = ["d"]; value = "paramecium"; } ]
-  flattenAttrs = flattenAttrsCond (const true);
-
-  # Like `flattenAttrs', but takes an additional predicate function that tells
-  # it whether to recurse into an attribute set.
-  flattenAttrsCond = cond: pipef [
+  # flattenAttrs (x: not (hasAttr "b" x)) { a = { b = 3; c = true; }; d = "paramecium"; }
+  # => [ { name = ["a"]; value = { b = 3; c = true; }; } { name = ["d"]; value = "paramecium"; } ]
+  flattenAttrs = cond: pipef [
     (mapAttrsToList (name: value: nameValuePair (singleton name) value))
     (foldr
       (p@{ name, value }: acc:
         if isAttrs value && cond value then
           acc ++ pipe value [
-            flattenAttrs
+            (flattenAttrs cond)
             (map (mapName (n: name ++ n)))
           ]
         else append p acc
@@ -44,8 +45,8 @@ with lib;
   # Applies the merge function to the output of flattenAttrs, producing an attrset.
   # The merge function converts each {name, value} pair to one in which the name is
   # merged into a single string, and may change the value as well.
-  flattenAttrsWith = merge: pipef [
-    flattenAttrs
+  flattenAttrsWith = cond: merge: pipef [
+    (flattenAttrs cond)
     (map (pipef [ merge nameValuePairToAttrs ]))
     recursiveConcat
   ];
