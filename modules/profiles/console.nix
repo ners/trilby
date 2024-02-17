ea3330b2b3dc7f81ef1d0519965eac22517c1bd9
ners <ners@gmx.ch>
2024-02-17 09:33:54 +0100

    base: introduce fhs module, remove console font

--- modules/profiles/console.nix
+++ modules/profiles/console.nix
@@ -7,10 +7,5 @@
     # Enable setting virtual console options as early as possible (in initrd).
     earlySetup = true;
     useXkbConfig = true;
-
-    font = pkgs.termify {
-      name = "cozette.psf.gz";
-      fontfile = "${pkgs.cozette}/share/fonts/misc/cozette.bdf";
-    };
   };
 }
