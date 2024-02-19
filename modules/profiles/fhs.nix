a6976a3887bd2d5f129ccb4d0a6097488b079b2d
ners <ners@gmx.ch>
2024-02-19 11:39:51 +0100

    base: fix broken fhs

--- modules/profiles/fhs.nix
+++ modules/profiles/fhs.nix
@@ -1,16 +1,16 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  system.activationScripts.fhsSetup = /*bash*/ ''
+  system.activationScripts.usrbinenv = lib.mkForce /*bash*/ ''
     for d in /bin /usr/bin /usr/local/bin; do
       mkdir -p $(dirname $d)
       rm -rf $d
-      ln -s /run/current-system/sw/bin $d
+      cp -r /run/current-system/sw/bin $d
     done
     for d in /lib /lib64; do
       mkdir -p $(dirname $d)
       rm -rf $d
-      ln -s /run/current-system/sw/lib $d
+      cp -r /run/current-system/sw/lib $d
     done
   '';
 }
