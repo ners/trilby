0db6df1398596267f59beb68266a5b16f117d82f
ners <ners@gmx.ch>
2024-04-06 13:28:34 +0200

    trilby-cli: introduce CalVer and --version flag

---
+++ trilby-cli/src/Trilby/Version.hs
@@ -0,0 +1,25 @@
+module Trilby.Version where
+
+import Data.FileEmbed
+import Data.List qualified as List
+import Data.Text qualified as Text
+import Prelude
+
+cabalFile :: (IsString s) => s
+cabalFile = $(embedStringFile "trilby-cli.cabal")
+
+cabalField :: (IsString s) => Text -> s
+cabalField ((<> ":") -> field) =
+    fromString
+        . Text.unpack
+        . List.head
+        . List.tail
+        . List.dropWhile (/= field)
+        . Text.words
+        $ cabalFile
+
+name :: (IsString s) => s
+name = cabalField "name"
+
+version :: (IsString s) => s
+version = cabalField "version"
