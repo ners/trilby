b184fd7f6e1f0efff1c997965cb3243740d653f2
ners <ners@gmx.ch>
2024-04-06 13:34:27 +0200

    base: set ELF loader in FHS module

--- modules/profiles/fhs.nix
+++ modules/profiles/fhs.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ trilby, lib, pkgs, ... }:
 
 {
   system.activationScripts.binsh = lib.mkForce ""; # obsolete
@@ -16,8 +16,19 @@
         fi
       done
     }
-    sw=/nix/var/nix/gcroots/current-system/sw
+    sw=/run/current-system/sw
     copyLinks $sw/bin /bin /usr/bin /usr/local/bin
     copyLinks $sw/lib /lib /lib64
   '';
+
+  environment = lib.mkMerge (
+    lib.optionals (lib.versionAtLeast trilby.release "24.05") [
+      {
+        ldso = "${pkgs.stdenv.cc.libc_lib}/lib64/ld-linux-x86-64.so.2";
+      }
+      (lib.optionalAttrs (trilby.hostSystem.cpu.name == "x86_64") {
+        ldso32 = "${pkgs.stdenv.cc.libc_lib}/lib/ld-linux-x86-64.so.2";
+      })
+    ]
+  );
 }
