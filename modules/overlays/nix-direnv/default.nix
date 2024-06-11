8fa5ed6e9bc1246fb87bdf85bbb88e59cb797ca1
ners <ners@gmx.ch>
2024-06-11 09:55:51 +0200

    base: update nix-direnv patch

--- modules/overlays/nix-direnv/default.nix
+++ modules/overlays/nix-direnv/default.nix
@@ -1,14 +1,19 @@
-{ ... }:
+{ lib, ... }:
 
 final: prev: {
-  nix-direnv = prev.nix-direnv.overrideAttrs (attrs: {
-    prePatch = ''
-      echo PATCHING
-      pwd
-      ls -alh
-    '';
-    patches = (attrs.patches or [ ]) ++ [
-      ./nvd.patch
-    ];
-  });
+  nix-direnv = prev.nix-direnv.override {
+    resholve = prev.resholve // {
+      mkDerivation = attrs: prev.resholve.mkDerivation (attrs // {
+        patches = (attrs.patches or [ ]) ++ [
+          ./nvd.patch
+        ];
+        solutions = lib.recursiveUpdate attrs.solutions {
+          default = {
+            inputs = with final; [ findutils nvd ] ++ attrs.solutions.default.inputs;
+            execer = [ "cannot:${final.nvd}/bin/nvd" ] ++ attrs.solutions.default.execer;
+          };
+        };
+      });
+    };
+  };
 }
