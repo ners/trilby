b15cb44c80694e8790f0174ab543f07165352c62
ners <ners@gmx.ch>
2024-12-02 22:44:54 +0100

    home: add mimeApps.inverted option

---
+++ modules/home/xdg.nix
@@ -0,0 +1,40 @@
+{ config, lib, ... }:
+
+with builtins;
+with lib;
+let
+  cfg = config.xdg.mimeApps.inverted;
+  addMime = desktopNames: mime: attrs: attrs // {
+    ${mime} = (attrs.${mime} or [ ]) ++ lib.splitString ";" desktopNames;
+  };
+  invert = with lib; foldlAttrs
+    (acc: desktopNames: mimes:
+      if isList mimes then foldr (addMime desktopNames) acc mimes
+      else addMime desktopName mimes acc
+    )
+    { };
+in
+{
+  options.xdg.mimeApps.inverted = {
+    defaultApplications = mkOption {
+      type = types.attrsOf (types.listOf types.str);
+      default = { };
+    };
+    addedAssociations = mkOption {
+      type = types.attrsOf (types.listOf types.str);
+      default = { };
+    };
+    removedAssociations = mkOption {
+      type = types.attrsOf (types.listOf types.str);
+      default = { };
+    };
+  };
+
+  config.xdg.mimeApps = {
+    defaultApplications = invert cfg.defaultApplications;
+    associations = {
+      added = invert cfg.addedAssociations;
+      removed = invert cfg.removedAssociations;
+    };
+  };
+}
