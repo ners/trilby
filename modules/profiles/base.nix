d36bd821c56d3d79f579091ff1f35301a3cc4f6d
ners <ners@gmx.ch>
2024-12-23 17:35:31 +0100

    darwin: share base with linux

--- modules/profiles/base.nix
+++ modules/profiles/base.nix
@@ -1,4 +1,4 @@
-{ config, inputs, trilby, lib, ... }:
+{ trilby, lib, ... }:
 
 with builtins;
 let
@@ -7,52 +7,6 @@ in
 {
   # Prefer our base config. This also prevents adding ZFS to `boot.supportedFilesystems` without forcing it.
   disabledModules = [ trilby.nixpkgs.nixosModules.profiles.base ];
-
-  imports = with inputs.self.nixosModules; [
-    profiles.bootloader
-    profiles.btrfs
-    profiles.console
-    profiles.documentation
-    profiles.fhs
-    profiles.getty
-    profiles.kernel
-    profiles.network
-    profiles.nix
-    profiles.nixpkgs
-    profiles.ssh
-    profiles.users
-    profiles.xfs
-    profiles.zram
-    profiles.zsh
-    inputs.disko.nixosModules.disko
-  ];
-
-  system.nixos = lib.mkMerge [
-    {
-      label = trilby.release;
-      inherit (lib.trivial) version;
-    }
-    (
-      lib.optionalAttrs (lib.versionAtLeast trilby.release "23.05") {
-        distroId = "${trilby.name}-${trilby.edition}";
-        distroName = lib.capitaliseWords "${trilby.name} ${trilby.edition}";
-      }
-    )
-  ];
-  system.systemBuilderArgs.name =
-    let
-      cn = trilby.configurationName;
-      sn = config.system.name;
-    in
-    if lib.hasInfix sn cn then cn else "${cn}-${sn}";
-
-  i18n.defaultLocale = lib.mkDefault "en_GB.UTF-8";
-
-  time.timeZone = lib.mkDefault "Europe/Zurich";
-
-  users.users.root.initialHashedPassword = "";
-
-  system.stateVersion = trilby.release;
 
   environment.systemPackages = with pkgs; [
     expect
@@ -67,6 +21,4 @@ in
     wget
     zip
   ];
-
-  programs.dconf.enable = true;
 }
