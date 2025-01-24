49c8f7c89c56171471fa5a48b12364f2b718fec4
Viktor Kleen <viktor@kleen.org>
2025-01-24 18:30:05 +0000

    Maybe this is what we want?

--- modules/overlays/pkgs/nvim.nix
+++ modules/overlays/pkgs/nvim.nix
@@ -1,21 +1,20 @@
 { inputs, system, lib, pkgs }@attrs:
 
 let
-  resolvedModules = lib.evalModules {
-    modules = [
-      # inputs.home-manager.nixosModules.default
-      inputs.nixvim.homeManagerModules.nixvim
-      {
-        programs.nixvim = {
-          luaLoader.enable = true;
-          extraConfigLuaPre = builtins.readFile ../../home/neovim/init.lua;
-        };
-      }
-    ];
-    # ++ lib.findModulesList ../../home/neovim/plugins;
-  };
+  resolvedModules =
+    {
+      imports = [
+        # inputs.home-manager.nixosModules.default
+        # inputs.nixvim.homeManagerModules.nixvim
+      ]; # ++ lib.findModulesList ../../home/neovim/plugins;
+      config = {
+        luaLoader.enable = true;
+        extraConfigLuaPre = builtins.readFile ../../home/neovim/init.lua;
+      };
+    };
+  # ++ lib.findModulesList ../../home/neovim/plugins;
 in
 inputs.nixvim.legacyPackages.${system}.makeNixvimWithModule {
-    inherit pkgs;
-    module = with builtins; trace "PLUGINS ${toJSON (attrNames resolvedModules.config)}" {}; # resolvedModules.config.programs.nixvim;
+  inherit pkgs;
+  module = resolvedModules; # with builtins; trace "PLUGINS ${toJSON (attrNames resolvedModules.config)}" { }; # resolvedModules.config.programs.nixvim;
 }
