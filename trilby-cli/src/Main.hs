5b64486e10ef88622d0b13650274b03866d0c1d3
ners <ners@gmx.ch>
2025-08-03 11:03:00 +0200

    trilby-cli: better locale selection in install

--- trilby-cli/src/Main.hs
+++ trilby-cli/src/Main.hs
@@ -6,7 +6,7 @@ import Trilby.Clean (clean)
 import Trilby.Command
 import Trilby.Infect (infect)
 import Trilby.Install (install)
-import Trilby.Install.Options (validateParsedInstallOpts)
+import Trilby.Install.Options (askLocale, validateParsedInstallOpts, askTimezone, askKeyboard)
 import Trilby.Log (withLog)
 import Trilby.Options
 import Trilby.Setup (setup)
@@ -21,6 +21,9 @@ main = withSystemTempDir "trilby" \tmpDir -> do
     let state = AppState{..}
     withLog state.verbosity do
         runApp state do
+            liftIO . print =<< askTimezone
+            liftIO . print =<< askLocale
+            liftIO . print =<< askKeyboard
             setup
             case opts.command of
                 Clean o -> clean o
