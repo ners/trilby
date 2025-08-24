dcab44790b70b84798fc7f05e38ee4e5b5522151
ners <ners@gmx.ch>
2025-08-24 12:41:11 +0200

    linux.polkit: enable and configure

--- modules/profiles/installer/default.nix
+++ modules/profiles/installer/default.nix
@@ -8,6 +8,8 @@
     })
   ];
 
+  users.users.nixos.enable = false;
+
   environment.systemPackages = with pkgs; [
     cryptsetup
     disko
@@ -41,4 +43,18 @@
   users.motd = builtins.readFile ./motd.txt;
 
   networking.hostName = "trilby";
+
+  # Whitelist wheel users to do anything
+  # This is useful for things like pkexec
+  #
+  # WARNING: this is dangerous for systems
+  # outside the installation-cd and shouldn't
+  # be used anywhere else.
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
