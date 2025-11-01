ea49759a9f83cafb0da7ea19d45298c3d1df6e93
ners <ners@gmx.ch>
2025-11-01 14:31:19 +0100

    polkit: update persist rules

--- modules/profiles/polkit.nix
+++ modules/profiles/polkit.nix
@@ -4,10 +4,15 @@
   # Cache admin credentials for 5 minutes
   security.polkit.extraConfig = ''
     polkit.addRule(function(action, subject) {
-        if ((action.id == "org.freedesktop.systemd1.manage-units") &&
-            subject.isInGroup("wheel")) {
-                return polkit.Result.AUTH_ADMIN_KEEP;
-        }
+      if (action.id == "org.freedesktop.policykit.exec") {
+        return polkit.Result.AUTH_ADMIN_KEEP;
+      }
+    });
+
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("org.freedesktop.systemd1.") == 0) {
+        return polkit.Result.AUTH_ADMIN_KEEP;
+      }
     });
   '';
 }
