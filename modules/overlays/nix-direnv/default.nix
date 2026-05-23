a65e63d11bf5f443fe7bb30cb14d0aae4444bfae
Cazim Hysi <cazim.hysi@gmail.com>
2026-05-23 14:08:27 +0200

    Update nix-direnv overlay to match new resholve function argument pattern in nixpkgs-unstable

--- modules/overlays/nix-direnv/default.nix
+++ modules/overlays/nix-direnv/default.nix
@@ -5,17 +5,19 @@ if unstable then
   {
     nix-direnv = prev.nix-direnv.override {
       resholve = prev.resholve // {
-        mkDerivation = attrs: prev.resholve.mkDerivation (attrs // {
-          patches = (attrs.patches or [ ]) ++ [
-            ./nvd.patch
-          ];
-          solutions = prev.lib.recursiveUpdate attrs.solutions {
-            default = {
-              inputs = with final; [ findutils nvd ] ++ attrs.solutions.default.inputs or [ ];
-              execer = [ "cannot:${final.nvd}/bin/nvd" ] ++ attrs.solutions.default.execer or [ ];
+        mkDerivation = f: prev.resholve.mkDerivation (finalAttrs:
+          let attrs = f finalAttrs;
+          in attrs // {
+            patches = (attrs.patches or [ ]) ++ [
+              ./nvd.patch
+            ];
+            solutions = prev.lib.recursiveUpdate attrs.solutions {
+              default = {
+                inputs = with final; [ findutils nvd ] ++ attrs.solutions.default.inputs or [ ];
+                execer = [ "cannot:${final.nvd}/bin/nvd" ] ++ attrs.solutions.default.execer or [ ];
+              };
             };
-          };
-        });
+          });
       };
     };
   }
